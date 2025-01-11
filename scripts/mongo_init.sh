dd629be71b5def22ce192cb55c1960b68277f693
Ali Ramezani <127216595+RamezaniAli@users.noreply.github.com>
2025-01-11 12:48:10 +0330

    Update mongo_init.sh

--- scripts/mongo_init.sh
+++ scripts/mongo_init.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
-set -e
+set -e 
 
-echo "=== Starting MongoDB data restoration process ==="
+DATABASE_NAME="utube"
+CORRECT_COUNT=6328629
+DUMP_DIRECTORY="/backup"
 
-COLLECTIONS_COUNT=$(mongo youtube --quiet --eval "db.getCollectionNames().length")
+COLLECTIONS_COUNT=$(mongo --host localhost \
+    -u "utube" \
+    -p "utube" \
+    --authenticationDatabase admin \
+    "$DATABASE_NAME" \
+    --quiet --eval "db.videos.count()")
 
 if [ "$COLLECTIONS_COUNT" -eq 0 ]; then
     echo "Database is empty. Starting data restoration..."
-    # Restore data using mongorestore
-    mongorestore --db youtube --verbose /dump
-    echo "=== MongoDB data restoration completed ==="
+    mongorestore --host localhost \
+        -u "utube" \
+        -p "utube" \
+        --authenticationDatabase admin \
+        --db "$DATABASE_NAME" \
+        --numInsertionWorkersPerCollection 4 \
+        --verbose "$DUMP_DIRECTORY"
+
+    COLLECTIONS_COUNT=$(mongo --host localhost \
+        -u "utube" \
+        -p "utube" \
+        --authenticationDatabase admin \
+        "$DATABASE_NAME" \
+        --quiet --eval "db.videos.count()")
+    echo "=== $COLLECTIONS_COUNT video added to MongoDB"
+    
+elif [ "$COLLECTIONS_COUNT" -ne "$CORRECT_COUNT" ]; then
+    echo "Database contains incorrect number of documents. Cleaning and restoring data..."
+    mongo --host localhost \
+        -u "utube" \
+        -p "utube" \
+        --authenticationDatabase admin \
+        "$DATABASE_NAME" \
+        --quiet --eval "db.dropDatabase()"
+    echo "Database cleaned. Restoring..."
+    mongorestore --host localhost \
+        -u "utube" \
+        -p "utube" \
+        --authenticationDatabase admin \
+        --db "$DATABASE_NAME" \
+        --numInsertionWorkersPerCollection 4 \
+        --verbose "$DUMP_DIRECTORY"
+    echo "=== MongoDB data restoration completed after cleaning ==="
 else
-    echo "Database already contains data. Skipping restoration."
+    echo "Database contains the correct number of documents. Skipping restoration."
 fi
