5d284ef1187309af16976657c1ce08385837e38d
Ali Ramezani <127216595+RamezaniAli@users.noreply.github.com>
2025-01-12 00:16:38 +0330

    Update mongo_init.sh

--- scripts/mongo_init.sh
+++ scripts/mongo_init.sh
@@ -1,52 +1,19 @@
 #!/bin/bash
-set -e 
+set -e
 
-DATABASE_NAME="utube"
-CORRECT_COUNT=6328629
-DUMP_DIRECTORY="/backup"
+until mongo --eval 'db.runCommand({ ping: 1 })' &>/dev/null; do
+  sleep 1
+done
 
-COLLECTIONS_COUNT=$(mongo --host localhost \
-    -u "utube" \
-    -p "utube" \
-    --authenticationDatabase admin \
-    "$DATABASE_NAME" \
-    --quiet --eval "db.videos.count()")
 
-if [ "$COLLECTIONS_COUNT" -eq 0 ]; then
-    echo "Database is empty. Starting data restoration..."
-    mongorestore --host localhost \
-        -u "utube" \
-        -p "utube" \
-        --authenticationDatabase admin \
-        --db "$DATABASE_NAME" \
-        --numInsertionWorkersPerCollection 4 \
-        --verbose "$DUMP_DIRECTORY"
+MONGO_USERNAME="${MONGO_INITDB_ROOT_USERNAME}"
+MONGO_PASSWORD="${MONGO_INITDB_ROOT_PASSWORD}"
+AUTH_STRING=""
 
-    COLLECTIONS_COUNT=$(mongo --host localhost \
-        -u "utube" \
-        -p "utube" \
-        --authenticationDatabase admin \
-        "$DATABASE_NAME" \
-        --quiet --eval "db.videos.count()")
-    echo "=== $COLLECTIONS_COUNT video added to MongoDB"
-    
-elif [ "$COLLECTIONS_COUNT" -ne "$CORRECT_COUNT" ]; then
-    echo "Database contains incorrect number of documents. Cleaning and restoring data..."
-    mongo --host localhost \
-        -u "utube" \
-        -p "utube" \
-        --authenticationDatabase admin \
-        "$DATABASE_NAME" \
-        --quiet --eval "db.dropDatabase()"
-    echo "Database cleaned. Restoring..."
-    mongorestore --host localhost \
-        -u "utube" \
-        -p "utube" \
-        --authenticationDatabase admin \
-        --db "$DATABASE_NAME" \
-        --numInsertionWorkersPerCollection 4 \
-        --verbose "$DUMP_DIRECTORY"
-    echo "=== MongoDB data restoration completed after cleaning ==="
-else
-    echo "Database contains the correct number of documents. Skipping restoration."
+if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]; then
+  AUTH_STRING="--username $MONGO_USERNAME --password $MONGO_PASSWORD --authenticationDatabase admin"
 fi
+
+mongorestore $AUTH_STRING --drop --dir /dump --numInsertionWorkersPerCollection=5
+
+echo "Done!"
