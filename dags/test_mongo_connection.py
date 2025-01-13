c236ce54f9e1c65d5b76fe1c7b96baa6d679849f
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-13 19:06:35 +0330

    ✅ add record_count to mongo_record_count_dag

--- dags/test_mongo_connection.py
+++ dags/test_mongo_connection.py
@@ -1,20 +1,16 @@
 from airflow import DAG
+from airflow.providers.mongo.hooks.mongo import MongoHook
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
 
 # Callbacks
 def process_data_from_mongo(**kwargs):
-    try:
-        from airflow.providers.mongo.hooks.mongo import MongoHook
-        print("MongoHook importing Done........................")
-    except ImportError as e:
-        print(f"Error importing MongoHook: {e}")
-    # conn_id = kwargs['conn_id']
-    # hook = MongoHook(mongo_conn_id=conn_id)
-    # collection = hook.get_collection('utube', 'videos')
-    # record_count = collection.count()
-    return "OLTP Mongo Connection ID!!"
+    conn_id = kwargs['conn_id']
+    hook = MongoHook(mongo_conn_id=conn_id)
+    collection = hook.get_collection('utube', 'videos')
+    record_count = collection.count_documents({})
+    return record_count
 
 
 # DAG and its tasks
@@ -29,4 +25,5 @@ with DAG(
     process_data_task = PythonOperator(
         task_id='process_data_from_mongo_task',
         python_callable=process_data_from_mongo,
+        op_kwargs={'conn_id': 'oltp_mongo_conn'}
     )
