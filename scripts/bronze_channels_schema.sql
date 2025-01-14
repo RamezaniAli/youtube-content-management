d5b74c47b0ec2c131b249c2a934438b71a9e658a
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-14 20:17:53 +0330

    ✨ add bronze_videos_schema script

--- scripts/bronze_channels_schema.sql
+++ scripts/bronze_channels_schema.sql
@@ -1,7 +1,26 @@
-CREATE TABLE IF NOT EXISTS bronze.channels_test (
-    id String,
-    created_at DateTime64(3, 'UTC') DEFAULT toDateTime64(0, 3, 'UTC')
+CREATE TABLE IF NOT EXISTS bronze.channels_test
+(
+    id                      String,  
+    username                String,  
+    userid                  String,  
+    avatar_thumbnail        String, 
+    is_official             Nullable(UInt8), 
+    name                    String,  
+    bio_links               String,  
+    total_video_visit       Int64,  
+    video_count             Int32,  
+    start_date              DateTime('UTC'),  
+    start_date_timestamp    Int64,  
+    followers_count         Nullable(Int64),  
+    following_count         Nullable(Int64), 
+    country                 LowCardinality(String), 
+    platform                LowCardinality(String), 
+    created_at              Nullable(DateTime('UTC')),  
+    update_count            Int32,
+    --these two added:
+    _source                 String DEFAULT 'postgres',
+    _ingestion_ts           DateTime DEFAULT now(),
 )
-ENGINE = MergeTree
-PRIMARY KEY (id)
-ORDER BY (id);
+ENGINE = MergeTree()
+PARTITION BY toYYYYMM(created_at)
+ORDER BY (userid);
---
+++ scripts/bronze_videos_schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS bronze.videos
+(
+    id                  UInt64,
+    owner_username      String,
+    owner_id            String,
+    title               String,
+    tags                Nullable(String),
+    uid                 String,
+    visit_count         UInt64,
+    owner_name          String,
+    poster              String,
+    owener_avatar       Nullable(String), -- typo: owner_avatar
+    duration            Nullable(UInt64),
+    posted_date         DateTime('UTC'),
+    posted_timestamp    UInt64,
+    sdate_rss           DateTime('UTC'),
+    sdate_rss_tp        UInt64,
+    comments            Nullable(String),
+    frame               Nullable(String),
+    like_count          Nullable(UInt64),
+    description         Nullable(String),
+    is_deleted          UInt8,
+    created_at          DateTime('UTC'),
+    expire_at           DateTime('UTC'),
+    is_produce_to_kafka UInt8,
+    update_count        UInt32,
+
+    _ingestion_ts       DateTime('UTC') DEFAULT now(),
+    _source             String DEFAULT 'mongo',
+    _raw_object         String
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMM(created_at)     
+ORDER BY (owner_id, created_at);
