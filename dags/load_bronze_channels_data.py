e51b5a048fa6fae48fb55a706fad3ce0de9432b4
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-15 12:10:45 +0330

    🗃️ change channels.is_official type

--- dags/load_bronze_channels_data.py
+++ dags/load_bronze_channels_data.py
@@ -85,7 +85,7 @@ def etl_data_from_postgres(**kwargs):
         ))
     # Execute the insert query
     clickhouse_client.insert(
-        'channels_test_4',
+        'channels_test',
         data_to_insert,
         column_names=clickhouse_channels_column_names
     )
