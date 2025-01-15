eca3d5b9aea9811bcbccd494b5c5e9f94412b536
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-15 11:32:17 +0330

    ✨add data_insertion to etl_data_from_postgres task

--- dags/load_bronze_channels_data.py
+++ dags/load_bronze_channels_data.py
@@ -48,17 +48,47 @@ def etl_data_from_postgres(**kwargs):
         password=clickhouse_password,
         database=clickhouse_database
     )
+    clickhouse_channels_column_names = [
+        'id', 'username', 'userid', 'avatar_thumbnail', 'is_official',
+        'name', 'bio_links', 'total_video_visit', 'video_count', 'start_date',
+        'start_date_timestamp', 'followers_count', 'following_count',
+        'country', 'platform', 'created_at', 'update_count'
+    ]
     # Connect to PostgreSQL
     pg_conn_id = kwargs['postgres_conn_id']
     pg_hook = PostgresHook(postgres_conn_id=pg_conn_id)
     # Prepare data for ClickHouse insertion
+    data_to_insert = []
     batch_size = 5
     skip = 0
     sql_query = f"SELECT * FROM channels LIMIT {batch_size} OFFSET {skip}"
     records = pg_hook.get_records(sql_query)
     for record in records:
-        print(record)
+        data_to_insert.append((
+            record[0],  # _id
+            record[1],  # username
+            record[2],  # userid
+            record[3],  # avatar_thumbnail
+            record[4],  # is_official
+            record[5],  # name
+            record[6],  # bio_links
+            record[7],  # total_video_visit
+            record[8],  # video_count
+            record[9],  # start_date
+            record[10],  # start_date_timestamp
+            record[11],  # followers_count
+            record[12],  # following_count
+            record[13],  # country
+            record[14],  # platform
+            record[15],  # created_at
+            record[16],  # update_count
+        ))
     # Execute the insert query
+    clickhouse_client.insert(
+        'channels_test',
+        data_to_insert,
+        column_names=clickhouse_channels_column_names
+    )
     return 'Done!'
 
 
