c1f3ce1f0776e84abccf6baa40e4524250ee7ab3
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-15 09:53:19 +0330

    🗃️ set default for bronze.channels is_official

--- scripts/bronze_channels_schema.sql
+++ scripts/bronze_channels_schema.sql
@@ -1,26 +1,25 @@
 CREATE TABLE IF NOT EXISTS bronze.channels_test
 (
-    id                      String,  
-    username                String,  
-    userid                  String,  
-    avatar_thumbnail        String, 
-    is_official             Nullable(UInt8), 
-    name                    String,  
-    bio_links               String,  
-    total_video_visit       Int64,  
-    video_count             Int32,  
-    start_date              DateTime('UTC'),  
-    start_date_timestamp    Int64,  
-    followers_count         Nullable(Int64),  
-    following_count         Nullable(Int64), 
-    country                 LowCardinality(String), 
-    platform                LowCardinality(String), 
-    created_at              Nullable(DateTime('UTC')),  
+    id                      String PRIMARY KEY,
+    username                String,
+    userid                  String,
+    avatar_thumbnail        String,
+    is_official             UInt8 DEFAULT 0,
+    name                    String,
+    bio_links               String,
+    total_video_visit       Int64,
+    video_count             Int32,
+    start_date              DateTime('UTC'),
+    start_date_timestamp    Int64,
+    followers_count         Nullable(Int64),
+    following_count         Nullable(Int64),
+    country                 LowCardinality(String),
+    platform                LowCardinality(String),
+    created_at              Nullable(DateTime('UTC')),
     update_count            Int32,
     --these two added:
     _source                 String DEFAULT 'postgres',
     _ingestion_ts           DateTime DEFAULT now(),
 )
 ENGINE = MergeTree()
-PARTITION BY toYYYYMM(created_at)
 ORDER BY (userid);
