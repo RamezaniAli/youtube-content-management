1c85943ffd8dc5719c5078a9037778f43c400c5b
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-16 13:05:56 +0330

    ✨ update gold_schema

--- scripts/gold_schema.sql
+++ scripts/gold_schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS gold.channel_growth
 )
 ENGINE = AggregatingMergeTree()
 PARTITION BY toYYYYMM(snapshot_date)
-ORDER BY (channel_userid, snapshot_date);
+ORDER BY (channel_userid, snapshot_date, channel_country);
 
 
 -- top performing channels
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS gold.top_performing_channels
 )
 ENGINE = AggregatingMergeTree()
 PARTITION BY toYYYYMM(snapshot_date)
-ORDER BY (channel_userid, snapshot_date);
+ORDER BY (channel_userid, highest_video_visits, highest_followers_count, highest_video_count);
 
 -- video engagement metrics
 
@@ -49,17 +49,16 @@ CREATE TABLE IF NOT EXISTS gold.video_engagement_metrics
     video_title             String,
     channel_userid          String,
     channel_name            String,
-    video_posted_date       Date,
+    video_posted_date       DateTime('UTC'),
 
-    visit_count_state       AggregateFunction(sum, UInt64),
-    like_count_state        AggregateFunction(sum, UInt64),
-    comment_count_state     AggregateFunction(sum, UInt64),
+    total_visits_state         AggregateFunction(sum, UInt64),
+    avg_engagement_rate_state  AggregateFunction(avg, Float32),
 
     gold_ingestion_ts       DateTime64(3, 'UTC') DEFAULT now()
 )
 ENGINE = AggregatingMergeTree()
 PARTITION BY toYYYYMM(video_posted_date)
-ORDER BY (video_uid, video_posted_date);
+ORDER BY (video_uid, avg_engagement_rate_state, video_posted_date);
 
 
 -- content popularity analysis
@@ -68,7 +67,6 @@ CREATE TABLE IF NOT EXISTS gold.content_popularity_analysis
 (
     video_uid               String,
     video_title             String,
-    video_tags             Array(String),
     visit_count_state       AggregateFunction(sum, UInt64),
 
     gold_ingestion_ts       DateTime64(3, 'UTC') DEFAULT now()
@@ -82,28 +80,36 @@ ORDER BY (video_uid, gold_ingestion_ts);
 
 CREATE TABLE IF NOT EXISTS gold.geographic_distribution_of_channels
 (
-    country                LowCardinality(String),
+    channel_country        LowCardinality(String),
+    channel_region         LowCardinality(String),
     total_followers_count  AggregateFunction(sum, Int64),
     total_video_visits     AggregateFunction(sum, UInt64),
+    total_channels_state   AggregateFunction(count, String),
 
     gold_ingestion_ts      DateTime64(3, 'UTC') DEFAULT now()
 )
 ENGINE = AggregatingMergeTree()
-PARTITION BY toYYYYMM(gold_ingestion_ts)
-ORDER BY (country, gold_ingestion_ts);
+PARTITION BY toYYYYMM(channel_region, gold_ingestion_ts)
+ORDER BY (channel_region, country, gold_ingestion_ts);
 
 
 -- channel activity and update trends
 
 CREATE TABLE IF NOT EXISTS gold.channel_activity_and_update_trends
 (
-    channel_userid          String,
-    update_count_state      AggregateFunction(sum, Int32),
+    channel_userid              String,
+    channel_name                String,
+    channel_country             LowCardinality(String),
+    snapshot_date               Date,
+
+    total_update_count_state    AggregateFunction(sum, Int32),
+    avg_video_age_state         AggregateFunction(avg, Int32),
+    total_videos_state          AggregateFunction(sum, Int32),
+    is_trending_state           AggregateFunction(sum, Int32),
     channel_start_date      DateTime64(3, 'UTC'),
-    channel_created_at      DateTime64(3, 'UTC'),
 
     gold_ingestion_ts       DateTime64(3, 'UTC') DEFAULT now()
 )
 ENGINE = AggregatingMergeTree()
 PARTITION BY toYYYYMM(gold_ingestion_ts)
-ORDER BY (channel_userid, gold_ingestion_ts);
+ORDER BY (channel_country, channel_userid, gold_ingestion_ts);
