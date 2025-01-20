9f9533d7b446361f0dc8dd13de77d3d3fef2d826
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-20 18:01:31 +0330

    🐛 update test dag dependencies

--- dags/check_bronze_channels_data.py
+++ dags/check_bronze_channels_data.py
@@ -82,6 +82,6 @@ with DAG(
         task_id='dummy_task'
     )
 
-    count_channels_records_task >> branch_task >> [etl_data_from_postgres_task, skip_etl_task]
+    count_channels_records_task >> branch_task >> [etl_data_from_postgres_task, skip_etl_task] >> dummy_task
     etl_data_from_postgres_task >> dummy_task
     skip_etl_task >> dummy_task
