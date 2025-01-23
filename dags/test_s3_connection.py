3784e707fd5d746ed3716f1be4ed1d25f8486f77
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-23 19:56:04 +0330

    🔊 add prints to list_arvan_objects callback

--- dags/test_s3_connection.py
+++ dags/test_s3_connection.py
@@ -5,24 +5,25 @@ from airflow.utils.dates import days_ago
 
 
 # Callbacks
-def list_arvan_buckets():
+def list_arvan_objects():
     s3_hook = S3Hook(aws_conn_id='arvan_s3_conn')
-    buckets = s3_hook.list_buckets()
-    print(f"Buckets in ArvanCloud: {buckets}")
-    return buckets
+    bucket = s3_hook.get_bucket(bucket_name='qbc')
+    print(f"Bucket Name: {bucket.name}")
+    for obj in bucket.objects.all():
+        print(f" - {obj.key} (Size: {obj.size} bytes)")
 
 
 # DAG and its tasks
 with DAG(
-    dag_id='list_arvan_buckets_dag',
+    dag_id='test_s3_connection_dag',
     schedule_interval=None,
     start_date=days_ago(1),
     catchup=False
 ) as dag:
 
-    list_buckets_task = PythonOperator(
-        task_id='list_arvan_buckets',
-        python_callable=list_arvan_buckets,
+    list_bucket_objects_task = PythonOperator(
+        task_id='list_bucket_objects_task',
+        python_callable=list_arvan_objects,
     )
 
-    list_buckets_task
+    list_bucket_objects_task
