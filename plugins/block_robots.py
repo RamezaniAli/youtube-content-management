65dda70640a3b93eb6b7fede8a63374495773418
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-01-23 20:58:21 +0330

    🐛 update block_robots plugin

--- plugins/block_robots.py
+++ plugins/block_robots.py
@@ -1,16 +1,14 @@
-from flask import Flask, request, abort
+from flask import request, abort
 from airflow.plugins_manager import AirflowPlugin
 
 
-def block_robots_middleware(app: Flask):
-    @app.before_request
-    def block_robots():
-        if request.path == '/robots.txt':
-            abort(403)
+def block_robots():
+    if request.path == '/robots.txt':
+        abort(403)
 
 
 class BlockRobotsPlugin(AirflowPlugin):
     name = "block_robots"
 
-    def on_load(self, app: Flask):
-        block_robots_middleware(app)
+    def on_load(self, app):
+        app.before_request(block_robots)
