ddf40f80a96d8a9fb74ef4c24b362dc8d1687c4f
Ali Ramezani <127216595+RamezaniAli@users.noreply.github.com>
2025-01-31 01:24:00 +0330

    Update bronze_channels_schema.sql base new data(null values)

--- scripts/bronze_channels_schema.sql
+++ scripts/bronze_channels_schema.sql
@@ -1,28 +1,29 @@
 CREATE TABLE IF NOT EXISTS bronze.channels
 (
     id                      String,
-    username                String,
-    userid                  String,
-    avatar_thumbnail        String,
+    username                Nullable(String),
+    userid                  Nullable(String),
+    avatar_thumbnail        Nullable(String),
     is_official             Nullable(UInt8),
-    name                    String,
-    bio_links               String,
-    total_video_visit       Int64,
-    video_count             Int32,
-    start_date              DATE,
-    start_date_timestamp    Int64,
+    name                    Nullable(String),
+    bio_links               Nullable(String),
+    total_video_visit       Nullable(Int64),
+    video_count             Nullable(Int32),
+    start_date              Nullable(DATE),
+    start_date_timestamp    Nullable(Int64),
     followers_count         Nullable(UInt64),
     following_count         Nullable(UInt64),
-    is_deleted              UInt8,
-    country                 LowCardinality(String),
-    platform                LowCardinality(String),
+    is_deleted              Nullable(UInt8),
+    country                 Nullable(String),
+    platform                Nullable(String),
     created_at              Nullable(DateTime('UTC')),
     updated_at              Nullable(DateTime('UTC')),
-    update_count            UInt32,
+    update_count            Nullable(UInt32),
     offset                  UInt64,
     --these two added:
     _source                 String DEFAULT 'postgres',
     _ingestion_ts           DateTime DEFAULT now(),
 )
 ENGINE = MergeTree()
-ORDER BY (id, userid);
+ORDER BY (id);
+
