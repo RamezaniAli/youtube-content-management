5a10c1c5e582be45a360884700f250a6e63d9515
Atieh <kasiri.atieh@food.app>
2025-01-30 15:49:31 +0330

    add incremental_loading and telegram_alerting

--- dags/load_bronze_inc_data.py
+++ dags/load_bronze_inc_data.py
@@ -16,16 +16,14 @@ def send_telegram_message(text,chat_id,bot_token):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {"chat_id": chat_id, "text": text}
     response = requests.post(url, json=payload)
+    if response.status_code != 200:
+        raise Exception(f"Failed to send Telegram message: {response.text}")
 
 def test_telegram_alert(**kwargs):
     chat_id = Variable.get("dag_alerting_chat_id")
     bot_token = Variable.get("dag_alerting_telegram_bot_token")
-    send_telegram_message("🚀 Test message from Airflow!", chat_id, bot_token)
-#def alert(context):
-#    send_telegram_message(
-#        text=f"""\U0001F534 Dag (( {context['dag'].dag_id} )) is failed.\U0001F534""",
-#        chat_id=Variable.get("dag_alerting_chat_id"),
-#        bot_token=Variable.get("dag_alerting_telegram_bot_token"))
+    send_telegram_message("🚀 Incremental ETL alerting from Airflow!", chat_id, bot_token)
+
 
 # Incremental extraction marker management
 def get_pg_last_execution():
@@ -41,9 +39,6 @@ def get_mg_last_execution():
 def update_mg_last_execution(**kwargs):
     mg_latest_execution = kwargs['ti'].xcom_pull(task_ids='etl_mongo_task')
     Variable.set("etl_mg_last_execution", mg_latest_execution)
-
-def print_context_info():
-    context = get_current_context()
 
 
 # ETL tasks
@@ -136,7 +131,7 @@ def etl_mongo(**kwargs):
     mongo_hook = MongoHook(mongo_conn_id)
     mongo_db = mongo_hook.get_conn()['utube']
     mongo_videos = mongo_db['videos']
-    query = {"offset": {"$gt": '6328627'}}
+    query = {"offset": {"$gt": mg_last_execution}}
     documents = list(mongo_videos.find(query))
 
     clickhouse_videos_column_names = [
@@ -221,7 +216,6 @@ with DAG(
         schedule_interval="30 23 * * *",
         start_date=pendulum.datetime(2025, 1, 25, tz="Asia/Tehran"),
         catchup=False,
-        #on_failure_callback=alert
 ) as dag:
 
     get_pg_last_execution_task = PythonOperator(
@@ -257,7 +251,8 @@ with DAG(
         op_kwargs={
             'postgres_conn_id': 'oltp_postgres_conn',
             'clickhouse_conn_id': 'wh_clickhouse_conn'
-        }
+        },
+        on_failure_callback=test_telegram_alert
     )
 
     etl_mongo_task = PythonOperator(
@@ -267,20 +262,10 @@ with DAG(
         op_kwargs={
             'mongo_conn_id': 'oltp_mongo_conn',
             'clickhouse_conn_id': 'wh_clickhouse_conn'
-        }
-    )
-    context_info_task = PythonOperator(
-        task_id="context_info_task",
-        python_callable=print_context_info,
-        provide_context=True,
-    )
-    telegram_message_task = PythonOperator(
-        task_id="telegram_message_task",
-        python_callable=test_telegram_alert,
-        provide_context=True,
+        },
+        on_failure_callback=test_telegram_alert
     )
 
     # Task dependencies
-    telegram_message_task
-    context_info_task >> get_pg_last_execution_task >> etl_postgres_task >> update_pg_last_execution_task
-    context_info_task >> get_mg_last_execution_task >> etl_mongo_task >> update_mg_last_execution_task
+    get_pg_last_execution_task >> etl_postgres_task >> update_pg_last_execution_task
+    get_mg_last_execution_task >> etl_mongo_task >> update_mg_last_execution_task
